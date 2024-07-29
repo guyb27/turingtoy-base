dd9f75464046ba9ea7a76efa013f1004992d5e2b
guyb27 <guillaume.madec@ecole2600.com>
2024-07-29 11:29:54 -0400

    passe les trois tests, mais pas de coverage a 100%

--- src/turingtoy/__init__.py
+++ src/turingtoy/__init__.py
@@ -1,4 +1,3 @@
-import json
 from typing import (
     Dict,
     List,
@@ -16,61 +15,54 @@ def run_turing_machine(
     input_: str,
     steps: Optional[int] = None,
 ) -> Tuple[str, List, bool]:
-    #pass  # Implement the function
+    final_state = machine['final states']
+    actual_state = machine['start state']
+    transition_table = machine['table']
 
-    i = 10;
-    i_min = 10;
-    i_real = 10
-    history=[]
-    next_state = machine["start state"]
-    actual_state = next_state
-    my_input = machine["blank"] * 10 + input_
+    line=input_
+    i = 0
+    history = []
     step = 0
-    while (next_state not in machine["final states"]) and (steps is None or step < steps):
-        if i > len(my_input)-1:
-            my_input = my_input + str(machine["blank"])
-        actual_state = machine["table"][next_state][my_input[i]]
-
-
-        curr = {
-            "state": next_state,
-            "reading": my_input[i],
-            #"position": i-1000 if i-1000 >= 0 else 0,
-            "position": i_real,
-            "memory": my_input[i_min:].strip(machine["blank"]),
-            "transition": {
-            }
-        }
-        if "write" in actual_state:
-            my_input = my_input[0:i] + actual_state["write"] + my_input[i+1:]
-            curr["transition"]["write"]=actual_state["write"]
-        side = ""
-        if "R" in actual_state:
-            i+=1
-            if i_real <= len(input_)-1:
-                i_real+=1
-            side = "R"
-
-        elif "L" in actual_state:
-            if i_real > 0:
-                i_real -=1
-            i-=1
-            side = "L"
-        if isinstance(actual_state, dict):
-            curr["transition"][side]=actual_state[side]
-            next_state=actual_state[side]
+    while steps is None or step < steps:
+        if i >= len(line):
+            line= line+machine['blank']
+        if i < 0:
+            line=machine['blank'] + line
+            i = 0
+        actual_symbol = line[i]
+        if actual_state in transition_table and actual_symbol in transition_table[actual_state]:
+            transition = transition_table[actual_state][actual_symbol]
+            history.append({
+                "state": actual_state,
+                "reading": actual_symbol,
+                "position": i,
+                "memory": line,
+                "transition": transition
+            })
+            if isinstance(transition, dict):
+                if 'write' in transition:
+                    line = line[:i] + transition['write'] + line[i+1:]
+                if 'R' in transition:
+                    actual_state = transition['R']
+                    i += 1
+                if 'L' in transition:
+                    actual_state = transition['L']
+                    i -= 1
+            else:
+                if transition == "R":
+                    i += 1
+                if transition == "L":
+                    i -= 1
+            step += 1
+            if actual_state in final_state:
+                break
         else:
-            curr["transition"]=actual_state
-        history.append(curr)
-        #print (curr)
-        if i < i_min:
-            i_min=i
-        step += 1
-
-    halted = actual_state in machine["final states"]
-    print("INPUT:")
-    print(input_)
-    print("MY_OUTPUT:")
-    print("["+my_input.strip(machine["blank"])+"]")
-    #return (my_input[i_min:].strip(machine["blank"]), history, halted)
-    return (my_input.strip(machine["blank"]), history, halted)
+            history.append({
+                "state": actual_state,
+                "reading": actual_symbol,
+                "position": i,
+                "memory": line,
+                "transition": transition
+            })
+            break
+    return (line.strip(machine['blank']), history, actual_state in final_state)
--- test_turingtoy.sh
+++ test_turingtoy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+work_repo=~/2600/Exos/math/turingtoy-base
+
 
 cd /tmp
 rm -rf /tmp/test_turingtoy
 git clone https://gitlab.com/maths-2600/turingtoy-base.git test_turingtoy
 cd test_turingtoy
 source .devenv/bash_init.sh
-cp ~/2600/Exos/math/turingtoy-base/src/turingtoy/__init__.py src/turingtoy/__init__.py
+cp $work_repo/src/turingtoy/__init__.py src/turingtoy/__init__.py
 nox -s tests
 rm -rf test_turingtoy
